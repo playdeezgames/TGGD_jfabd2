3dba624fab1e99631543fae37bfd4254ec6f5066
ernestpazera <ernestpazera@gmail.com>
2022-02-05 05:52:13 -0600

    i shippped it, i submitted it, i put a cover image up.

--- TGGD_jfabd2/TGGD_jfabd2.Data/ItemData.vb
+++ TGGD_jfabd2/TGGD_jfabd2.Data/ItemData.vb
@@ -16,7 +16,12 @@
         End Using
         Return GetLastInsertRowId()
     End Function
-    Function ReadItemType(itemId As Integer) As Integer
-
+    Function ReadItemType(itemId As Integer) As Integer?
+        Initialize()
+        Using command = connection.CreateCommand()
+            command.CommandText = "SELECT [ItemType] FROM [Items] WHERE [ItemId]=@ItemId;"
+            command.Parameters.AddWithValue("@ItemId", itemId)
+            Return command.ExecuteScalar()
+        End Using
     End Function
 End Module

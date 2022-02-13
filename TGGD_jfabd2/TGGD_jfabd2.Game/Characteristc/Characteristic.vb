a7f590f6f36503d781c3dbab75bc0b37cfd5ab61
ernestpazera <ernestpazera@gmail.com>
2022-02-13 03:48:30 -0600

    added con and siz

--- TGGD_jfabd2/TGGD_jfabd2.Game/Characteristc/Characteristic.vb
+++ TGGD_jfabd2/TGGD_jfabd2.Game/Characteristc/Characteristic.vb
@@ -2,4 +2,8 @@
     Dexterity '15d6
     Charisma '15d6
     Willpower '15d6
+    Constitution '15d6
+    Size '30+10d6
+    'Intelligence '30+10d6
+    'Strength '15d6
 End Enum
--- TGGD_jfabd2/TGGD_jfabd2.Game/Characteristc/Characteristics.vb
+++ TGGD_jfabd2/TGGD_jfabd2.Game/Characteristc/Characteristics.vb
@@ -1,16 +1,20 @@
 ﻿Public Module Characteristics
     Function GenerateForCharacter(characteristic As Characteristic) As Integer
         Select Case characteristic
-            Case Characteristic.Dexterity, Characteristic.Charisma, Characteristic.Willpower
+            Case Characteristic.Dexterity, Characteristic.Charisma, Characteristic.Willpower, Characteristic.Constitution
                 Return Roll(15, 6)
+            Case Characteristic.Size
+                Return 30 + Roll(10, 6)
             Case Else
                 Throw New NotImplementedException()
         End Select
     End Function
     Function GenerateForCritter(characteristic As Characteristic, critterType As Integer) As Integer
         Select Case characteristic
-            Case Characteristic.Dexterity, Characteristic.Charisma, Characteristic.Willpower
+            Case Characteristic.Dexterity, Characteristic.Charisma, Characteristic.Willpower, Characteristic.Constitution
                 Return Roll(15, 6)
+            Case Characteristic.Size
+                Return 15 + Roll(10, 3)
             Case Else
                 Throw New NotImplementedException()
         End Select
@@ -23,6 +27,10 @@
                 Return "CHA"
             Case Characteristic.Willpower
                 Return "POW"
+            Case Characteristic.Constitution
+                Return "CON"
+            Case Characteristic.Size
+                Return "SIZ"
             Case Else
                 Throw New NotImplementedException()
         End Select

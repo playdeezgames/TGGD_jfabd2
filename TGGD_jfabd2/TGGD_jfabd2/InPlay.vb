02b00458ac06f2d07a7d87574516a2b47f258766
ernestpazera <ernestpazera@gmail.com>
2022-02-21 05:45:19 -0600

    got the primary buttons for the in play menu correctly enabled

--- TGGD_jfabd2/TGGD_jfabd2/InPlay.vb
+++ TGGD_jfabd2/TGGD_jfabd2/InPlay.vb
@@ -67,6 +67,39 @@ Module InPlay
             End If
         End While
     End Sub
+    Private Sub UpdateStatus(label As Label)
+        Dim character = New PlayerCharacter()
+        Dim location = character.GetLocation()
+        Dim hasCritter = location.HasCritters()
+        Dim tree = location.GetTree()
+        Dim canPickFruit = tree IsNot Nothing
+        Dim hasGroundInventory = Not location.GetInventory().IsEmpty()
+        Dim vendor = location.GetVendor()
+        Dim hasVendor = vendor IsNot Nothing
+        label.Text = ""
+        If character.IsAlive Then
+            label.Text += "Yer alive!
+"
+            If canPickFruit Then
+                label.Text += $"There is a {FruitTypes.GetName(tree.GetFruitType())} tree here.
+"
+            End If
+            If hasVendor Then
+                label.Text += "There is a vendor here.
+"
+            End If
+            If hasGroundInventory Then
+                label.Text += "There is some stuff on the ground.
+"
+            End If
+            If hasCritter Then
+                label.Text += "There is at least one critter here.
+"
+            End If
+        Else
+            label.Text += "Yer dead!"
+        End If
+    End Sub
     Sub Run()
         Dim menuButton As New Button("Menu")
         AddHandler menuButton.Clicked, Sub()
@@ -74,14 +107,35 @@ Module InPlay
                                                Application.RequestStop()
                                            End If
                                        End Sub
-        Dim dlg As New Dialog("Yer playin' the game!", menuButton)
-
+        Dim moveButton As New Button("Move...")
+        Dim turnButton As New Button("Turn...")
+        Dim statusButton As New Button("Status...")
+        Dim interactButton As New Button("Interact...")
         Dim statusLabel As New Label(New Rect(1, 1, 40, 5))
+
+        Dim dlg As New Dialog("Yer playin' the game!", moveButton, turnButton, statusButton, interactButton, menuButton)
         AddHandler dlg.KeyPress, Sub(args)
                                      If args.KeyEvent.Key = Key.Esc Then
                                          args.Handled = True
                                      End If
                                  End Sub
+        dlg.Add(statusLabel)
+        UpdateStatus(statusLabel)
+        UpdateButtons(moveButton, turnButton, statusButton, interactButton)
         Application.Run(dlg)
+    End Sub
+
+    Private Sub UpdateButtons(moveButton As Button, turnButton As Button, statusButton As Button, interactButton As Button)
+        Dim character = New PlayerCharacter()
+        Dim location = character.GetLocation()
+        Dim hasCritter = location.HasCritters()
+        Dim tree = location.GetTree()
+        Dim canPickFruit = tree IsNot Nothing
+        Dim hasGroundInventory = Not location.GetInventory().IsEmpty()
+        Dim vendor = location.GetVendor()
+        Dim hasVendor = vendor IsNot Nothing
+        moveButton.Enabled = character.IsAlive
+        turnButton.Enabled = character.IsAlive
+        interactButton.Enabled = character.IsAlive AndAlso (canPickFruit OrElse hasGroundInventory OrElse hasVendor OrElse hasCritter)
     End Sub
 End Module

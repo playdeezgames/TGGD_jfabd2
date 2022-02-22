3257f3e1b45ee6aa91c5be4cf36f37e17b3807db
ernestpazera <ernestpazera@gmail.com>
2022-02-22 04:18:27 -0600

    i can see the characteristics and the buttons on the status screen

--- TGGD_jfabd2/TGGD_jfabd2/Status/StatusMenu.vb
+++ TGGD_jfabd2/TGGD_jfabd2/Status/StatusMenu.vb
@@ -42,10 +42,38 @@ Module StatusMenu
             End Select
         End While
     End Sub
+    Private Sub AddCharacteristics(dlg As Dialog)
+        Dim character = New PlayerCharacter()
+        Dim stats = [Enum].GetValues(GetType(Characteristic))
+        Dim row = 1
+        For Each stat In stats
+            dlg.Add(New Label(1, row, $"{Characteristics.GetAbbreviation(CType(stat, Characteristic))} {character.GetCharacteristic(CType(stat, Characteristic))}"))
+            row += 1
+        Next
+    End Sub
     Sub Run()
         Dim cancelButton As New Button("Never mind")
         AddHandler cancelButton.Clicked, AddressOf Application.RequestStop
-        Dim dlg As New Dialog("Status", cancelButton)
+        Dim statisticsButton As New Button("Statistics...")
+        AddHandler statisticsButton.Clicked, Sub()
+
+                                             End Sub
+        Dim inventoryButton As New Button("Inventory...")
+        Dim equipmentButton As New Button("Equipment...")
+        Dim dlg As New Dialog("Status", cancelButton, statisticsButton, inventoryButton, equipmentButton)
+        Dim updateButtons = Sub()
+                                Dim character = New PlayerCharacter()
+                                inventoryButton.Enabled = Not character.GetInventory().IsEmpty()
+                                equipmentButton.Enabled = character.GetEquipment().Any()
+                            End Sub
+        AddHandler inventoryButton.Clicked, Sub()
+                                                updateButtons()
+                                            End Sub
+        AddHandler equipmentButton.Clicked, Sub()
+                                                updateButtons()
+                                            End Sub
+        updateButtons()
+        AddCharacteristics(dlg)
         Application.Run(dlg)
     End Sub
 End Module

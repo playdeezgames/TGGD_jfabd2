deedef2e7c7485fc4486e7d0a68715f54ac428f5
ernestpazera <ernestpazera@gmail.com>
2022-02-22 04:49:00 -0600

    i can unequip stuff

--- TGGD_jfabd2/TGGD_jfabd2/Status/Equipment/EquipSlotMenu.vb
+++ TGGD_jfabd2/TGGD_jfabd2/Status/Equipment/EquipSlotMenu.vb
@@ -1,7 +1,8 @@
-﻿Imports TGGD_jfabd2.Game
+﻿Imports Terminal.Gui
+Imports TGGD_jfabd2.Game
 
 Module EquipSlotMenu
-    Sub Run(equipSlot As EquipSlot)
+    Private Sub OldRun(equipSlot As EquipSlot)
         Dim done As Boolean = False
         While Not done
             Dim character As New PlayerCharacter()
@@ -32,5 +33,25 @@ Module EquipSlotMenu
                 End Select
             End If
         End While
+    End Sub
+    Sub Run(equipSlot As EquipSlot)
+        Dim cancelButton As New Button("Never mind")
+        AddHandler cancelButton.Clicked, AddressOf Application.RequestStop
+        Dim character As New PlayerCharacter()
+        Dim item = character.GetEquipment()(equipSlot)
+        Dim unequipButton As New Button("Unequip")
+        AddHandler unequipButton.Clicked, Sub()
+                                              item.Unequip()
+                                              Application.RequestStop()
+                                          End Sub
+        Dim critterButton As New Button("Critter...")
+        AddHandler critterButton.Clicked, Sub()
+                                              'TODO: go to critter item menu
+                                          End Sub
+        critterButton.Enabled = item.IsCritter
+        Dim dlg As New Dialog(EquipSlots.GetName(equipSlot), cancelButton, unequipButton, critterButton)
+        dlg.Add(New Label(1, 1, item.GetName()))
+        dlg.Add(New Label(1, 2, item.GetDescription()))
+        Application.Run(dlg)
     End Sub
 End Module
--- TGGD_jfabd2/TGGD_jfabd2/Status/Equipment/Equipment.vb
+++ TGGD_jfabd2/TGGD_jfabd2/Status/Equipment/Equipment.vb
@@ -13,41 +13,6 @@ Class EquipmentListItem
 End Class
 
 Module Equipment
-    Private Sub OldRun()
-        Dim done As Boolean
-        While Not done
-            Dim equipment = New PlayerCharacter().GetEquipment()
-            If Not equipment.Any() Then
-                Return
-            End If
-            ShowMenuTitle("Yer Equipment:")
-            Dim index = 1
-            Dim slots As New List(Of EquipSlot)
-            For Each entry In equipment
-                slots.Add(entry.Key)
-                ShowMenuItem($"{index}) ({EquipSlots.GetName(entry.Key)}){entry.Value.GetName()}")
-                index += 1
-            Next
-            ShowMenuItem("0) Never mind")
-            ShowPrompt()
-            Dim input = Console.ReadLine()
-            Select Case input
-                Case "0"
-                    done = True
-                Case Else
-                    If Integer.TryParse(input, index) Then
-                        index -= 1
-                        If index < slots.Count Then
-                            EquipSlotMenu.Run(slots(index))
-                        Else
-                            InvalidInput()
-                        End If
-                    Else
-                        InvalidInput()
-                    End If
-            End Select
-        End While
-    End Sub
     Function GetEquipmentList() As IList
         Return New PlayerCharacter().GetEquipment().Select(Function(entry)
                                                                Return New EquipmentListItem(entry.Key, entry.Value)
@@ -59,8 +24,13 @@ Module Equipment
         Dim dlg As New Dialog("Equipment", cancelButton)
         Dim listView As New ListView(New Rect(1, 1, 40, 22), GetEquipmentList())
         AddHandler listView.OpenSelectedItem, Sub(args)
-                                                  'TODO: go to equipment item screen
-                                                  listView.SetSource(GetEquipmentList())
+                                                  EquipSlotMenu.Run(CType(args.Value, EquipmentListItem).EquipSlot)
+                                                  Dim equipmentList = GetEquipmentList()
+                                                  If equipmentList.Count() > 0 Then
+                                                      listView.SetSource(equipmentList)
+                                                  Else
+                                                      Application.RequestStop()
+                                                  End If
                                               End Sub
         dlg.Add(listView)
         Application.Run(dlg)

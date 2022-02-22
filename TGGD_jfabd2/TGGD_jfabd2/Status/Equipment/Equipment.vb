d5328852e917c6b7726a1f829919ccffcc8d1879
ernestpazera <ernestpazera@gmail.com>
2022-02-22 04:35:58 -0600

    i can see the list of equipment

--- TGGD_jfabd2/TGGD_jfabd2/Status/Equipment/Equipment.vb
+++ TGGD_jfabd2/TGGD_jfabd2/Status/Equipment/Equipment.vb
@@ -1,5 +1,16 @@
 ﻿Imports Terminal.Gui
 Imports TGGD_jfabd2.Game
+Class EquipmentListItem
+    ReadOnly Property Item As Item
+    ReadOnly Property EquipSlot As EquipSlot
+    Sub New(equipSlot As EquipSlot, item As Item)
+        Me.Item = item
+        Me.EquipSlot = equipSlot
+    End Sub
+    Public Overrides Function ToString() As String
+        Return $"{EquipSlots.GetName(EquipSlot)}: {Item.GetName()}"
+    End Function
+End Class
 
 Module Equipment
     Private Sub OldRun()
@@ -37,10 +48,21 @@ Module Equipment
             End Select
         End While
     End Sub
+    Function GetEquipmentList() As IList
+        Return New PlayerCharacter().GetEquipment().Select(Function(entry)
+                                                               Return New EquipmentListItem(entry.Key, entry.Value)
+                                                           End Function).ToList()
+    End Function
     Sub Run()
         Dim cancelButton As New Button("Never mind")
         AddHandler cancelButton.Clicked, AddressOf Application.RequestStop
         Dim dlg As New Dialog("Equipment", cancelButton)
+        Dim listView As New ListView(New Rect(1, 1, 40, 22), GetEquipmentList())
+        AddHandler listView.OpenSelectedItem, Sub(args)
+                                                  'TODO: go to equipment item screen
+                                                  listView.SetSource(GetEquipmentList())
+                                              End Sub
+        dlg.Add(listView)
         Application.Run(dlg)
     End Sub
 End Module
--- TGGD_jfabd2/TGGD_jfabd2/Status/StatusMenu.vb
+++ TGGD_jfabd2/TGGD_jfabd2/Status/StatusMenu.vb
@@ -2,46 +2,6 @@
 Imports TGGD_jfabd2.Game
 
 Module StatusMenu
-    Private Sub OldRun()
-        Dim done = False
-        Dim character = New PlayerCharacter()
-        While Not done
-            ShowMenuTitle("Status:")
-            Dim stats = [Enum].GetValues(GetType(Characteristic))
-            For Each stat In stats
-                ShowInfo($"{Characteristics.GetAbbreviation(CType(stat, Characteristic))} {character.GetCharacteristic(CType(stat, Characteristic))}")
-            Next
-            ShowMenuItem("1) Statististics")
-            If Not character.GetInventory().IsEmpty() Then
-                ShowMenuItem($"2) Inventory")
-            End If
-            If character.GetEquipment().Any() Then
-                ShowMenuItem("3) Equipment")
-            End If
-            ShowMenuItem("0) Never mind")
-            ShowPrompt()
-            Select Case Console.ReadLine()
-                Case "0"
-                    done = True
-                Case "1"
-                    CharacterStatistics.Run()
-                Case "2"
-                    If Not character.GetInventory().IsEmpty() Then
-                        Inventory.Run()
-                    Else
-                        InvalidInput()
-                    End If
-                Case "3"
-                    If character.GetEquipment().Any() Then
-                        Equipment.Run()
-                    Else
-                        InvalidInput()
-                    End If
-                Case Else
-                    InvalidInput()
-            End Select
-        End While
-    End Sub
     Private Sub AddCharacteristics(dlg As Dialog)
         Dim character = New PlayerCharacter()
         Dim stats = [Enum].GetValues(GetType(Characteristic))
